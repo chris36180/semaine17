c64bcc7692fa75e0a2d9a220d8740292c31ef765
christophe GAVAUD <christophe.gavaud@gmail.com>
2016-02-24 07:49:12 +0000

    2nd

--- blog/db/migrate/20160205173059_table_articles.rb
+++
@@ -1,8 +0,0 @@
-class TableArticles < ActiveRecord::Migration
-  def change
-    create_table :articles
-    add_column :articles, :title, :string
-    add_column :articles, :content, :text
-  end
-end
-
